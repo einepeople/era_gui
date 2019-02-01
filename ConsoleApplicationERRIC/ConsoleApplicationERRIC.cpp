702d3b385a683180cd6920f221b9696c3953ed5b
d.romanov <d.romanov@innopolis.ru>
2019-02-02 00:45:04 +0300

    Synchornized naming convention

---
+++ ConsoleApplicationERRIC/ConsoleApplicationERRIC.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+
+using namespace System;
+using namespace System::ComponentModel;
+using namespace System::Collections;
+using namespace System::Windows::Forms;
+using namespace System::Data;
+using namespace System::Drawing;
+using namespace ConsoleApplicationERRIC;
+
+[STAThreadAttribute]
+int main(array<System::String ^> ^args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::Run(gcnew MyForm());
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# erric_gui
+GUI for ERRIC emulator
+
+# How to build
+You are requiered to have a builded [erric_interpreter](https://github.com/SuperPrower/era_interpreter) library.
+Open project in VS, add `include` and `erric_interpreter.lib` folders paths to the project, build the project,
+then move `erric_interpreter.dll` to `{path_to_project}/Debug`. You are ready to launch the program.
